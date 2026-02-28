03add0991b6d6676d674b2ccb762bb5d66e1a172
laila-khalid <13252@holbertonstudents.com>
2026-02-28 01:10:10 +0000

    2. I sometimes suffer from insomnia

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+		putchar(alp);
+	putchar('\n');
+
+	return (0);
+}
