873031cde3c7db899dcaa7effd085784ef768e59
laila-khalidâ <13252@holbertonstudents.com>
2026-03-04 06:10:34 +0000

    Add _putchar to main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -5,4 +5,12 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
 #endif
