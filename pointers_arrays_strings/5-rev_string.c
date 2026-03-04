818e8494e54127d264ef0524d7d705bb52888c64
laila-khalidâ <13252@holbertonstudents.com>
2026-03-04 06:31:05 +0000

    Task 5: reverse string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string to reverse
+ */
+void rev_string(char *s)
+{
+	int len = 0;
+	int i = 0;
+	char temp;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	len--;
+	while (i < len)
+	{
+		temp = s[i];
+		s[i] = s[len];
+		s[len] = temp;
+		i++;
+		len--;
+	}
+}
