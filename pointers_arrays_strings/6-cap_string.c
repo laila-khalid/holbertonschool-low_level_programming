49e99a320ffd48b0707b394745a9fcb8a0336bcc
laila-khalidâ <13252@holbertonstudents.com>
2026-03-04 08:38:49 +0000

    Task 6: cap_string

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: string to modify
+ *
+ * Return: pointer to s
+ */
+char *cap_string(char *s)
+{
+	int i = 0, j;
+	char sep[] = " \t\n,;.!?\"(){}";
+
+	if (s[i] >= 'a' && s[i] <= 'z')
+		s[i] -= 32;
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; sep[j] != '\0'; j++)
+		{
+			if (s[i] == sep[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] -= 32;
+				}
+			}
+		}
+		i++;
+	}
+
+	return (s);
+}
