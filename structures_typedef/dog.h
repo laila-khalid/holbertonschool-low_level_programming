950c3e4bfe02af53dc912112ac6f3cef8d967cd4
laila-khalid”git config --global user.email 13252@holbertonstudents.comgit config --global user.name laila-khalid” <13252@holbertonstudents.com>
2026-04-04 19:03:59 +0000

    Task 3: typedef dog_t

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -15,25 +15,6 @@ struct dog
 	float age;
 	char *owner;
 };
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-#ifndef DOG_H
-#define DOG_H
-
-/**
- * struct dog - a dog's basic info
- * @name: dog's name
- * @age: dog's age
- * @owner: dog's owner
- *
- * Description: defines a dog structure with name, age, and owner
- */
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
 
 /**
  * dog_t - typedef for struct dog
@@ -43,5 +24,4 @@ typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
-#endif
 #endif
