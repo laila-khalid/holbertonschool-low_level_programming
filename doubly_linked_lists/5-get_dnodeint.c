2c5fa92548a61e6c788a85345b953d0f0f468c12
laila-khalid”git config --global user.email 13252@holbertonstudents.comgit config --global user.name laila-khalid” <13252@holbertonstudents.com>
2026-04-25 20:27:22 +0000

    Task 5: get node at index

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list
+ * @head: pointer to the head of the list
+ * @index: index of the node, starting from 0
+ *
+ * Return: the nth node, or NULL if it failed
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	for (i = 0; head != NULL; i++)
+	{
+		if (i == index)
+			return (head);
+		head = head->next;
+	}
+
+	return (NULL);
+}
