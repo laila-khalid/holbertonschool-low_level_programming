55f5d727f7af8cf428f96db072c4b388798ec65a
laila-khalid”git config --global user.email 13252@holbertonstudents.comgit config --global user.name laila-khalid” <13252@holbertonstudents.com>
2026-04-25 21:02:19 +0000

    Fix Betty style: split functions

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -4,37 +4,50 @@
 #include <unistd.h>
 
 /**
+ * close_err - closes a file descriptor and checks for error
+ * @fd: the file descriptor to close
+ */
+void close_err(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
  * main - copies the content of a file to another file
- * @ac: argument count
- * @av: argument vector
+ * @ac: number of arguments
+ * @av: array of arguments
  *
  * Return: 0 on success
  */
 int main(int ac, char **av)
 {
-	int fd_from, fd_to, r, w;
-	char buffer[1024];
+	int f_from, f_to, r, w;
+	char buf[1024];
 
 	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fd_from = open(av[1], O_RDONLY);
-	if (fd_from == -1)
+	f_from = open(av[1], O_RDONLY);
+	if (f_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fd_to == -1)
+	f_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (f_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
-	while ((r = read(fd_from, buffer, 1024)) > 0)
+	while ((r = read(f_from, buf, 1024)) > 0)
 	{
-		w = write(fd_to, buffer, r);
+		w = write(f_to, buf, r);
 		if (w == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
@@ -46,13 +59,7 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	if (close(fd_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
-	}
-	if (close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
-	}
+	close_err(f_from);
+	close_err(f_to);
 	return (0);
 }
